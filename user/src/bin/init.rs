1bfc478439137805b31ef64ccd32f3823e5d32d6
kazukousen <mmchari.0228@gmail.com>
2022-10-01 19:14:18 +0900

    init, ls

--- user/src/bin/init.rs
+++ user/src/bin/init.rs
@@ -5,16 +5,16 @@
 #![reexport_test_harness_main = "test_main"]
 
 use xv6rs_user::{
+    entry_point,
     fcntl::O_RDWR,
     println,
-    syscall::{sys_dup, sys_exec, sys_exit, sys_fork, sys_mknod, sys_open, sys_wait},
+    syscall::{sys_dup, sys_exec, sys_fork, sys_mknod, sys_open, sys_wait},
+    Args,
 };
 
-#[no_mangle]
-extern "C" fn _start() {
-    #[cfg(test)]
-    crate::test_main();
+entry_point!(main);
 
+fn main(_: &mut Args) -> Result<i32, &'static str> {
     if sys_open("console\0", O_RDWR) < 0 {
         sys_mknod("console\0", 1, 1);
         sys_open("console\0", O_RDWR);
@@ -26,14 +26,12 @@ extern "C" fn _start() {
         println!("init: starting sh");
         let pid = sys_fork();
         if pid < 0 {
-            println!("init: fork failed");
-            sys_exit(1);
+            return Err("fork failed");
         }
         if pid == 0 {
             let cmd = "sh\0";
             sys_exec(cmd);
-            println!("init: exec failed");
-            sys_exit(1);
+            return Err("exec failed");
         }
 
         loop {
@@ -43,8 +41,7 @@ extern "C" fn _start() {
                 // the shell exited; restart it.
                 break;
             } else if wpid < 0 {
-                println!("init: wait returned an error");
-                sys_exit(1);
+                return Err("wait returned an error");
             }
             // it was a parentless process; do nothing.
         }
